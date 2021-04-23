e8010b1678c1ac4a3811e9b19217797111d858a3
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-04-23 16:24:54 -0600

    3 - Velocity And Jumping

--- dasher.cpp
+++ dasher.cpp
@@ -8,12 +8,28 @@ int main()
     // initialize the window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
 
         // stop drawing
         EndDrawing();
