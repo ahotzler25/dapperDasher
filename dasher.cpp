6e17b7f84a32eb0aba330d655e76f50e7fb9d9fb
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-04-30 11:16:06 -0600

    4 - Gravity And Acceleration

--- dasher.cpp
+++ dasher.cpp
@@ -8,6 +8,9 @@ int main()
     // initialize the window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    // acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+
     // rectangle dimensions
     const int width{50};
     const int height{80};
@@ -22,11 +25,24 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        // perform ground check
+        if (posY >= windowHeight - height)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+        }
+        else
+        {
+            // rectangle is in the air
+            velocity += gravity;
+        }
+
         if (IsKeyPressed(KEY_SPACE))
         {
             velocity -= 10;
         }
 
+        // update the position
         posY += velocity;
 
         DrawRectangle(windowWidth/2, posY, width, height, BLUE);
