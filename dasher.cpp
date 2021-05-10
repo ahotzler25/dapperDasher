baec64d7b9ce958ed6370a65d5db50d5f64c12c9
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-05-10 10:43:50 -0600

    13 - Multiple Nebula Hazards

--- dasher.cpp
+++ dasher.cpp
@@ -16,10 +16,17 @@ int main()
     Rectangle nebRec{0.0, 0.0, nebula.width/8, nebula.height/8};
     Vector2 nebPos{windowWidth, windowHeight - nebRec.height};
 
-    // nebual animation variables
+    Rectangle neb2Rec{0.0, 0.0, nebula.width/8, nebula.height/8};
+    Vector2 neb2Pos{windowWidth + 300, windowHeight - nebRec.height};
+
+    // nebula animation variables
     int nebFrame{};
     const float nebUpdateTime{1.0/12.0};
     float nebRunningTime{};
+
+    int neb2Frame{};
+    const float neb2UpdateTime{1.0/16.0};
+    float neb2RunningTime;
 
     // nebula X velocity (pixels/second)
     int nebVel{-200};
@@ -81,6 +88,9 @@ int main()
         // update nebula position
         nebPos.x += nebVel * dT;
 
+        // update the second nebula's position
+        neb2Pos.x += nebVel * dT;
+
         // update scarfy position
         scarfyPos.y += velocity * dT;
 
@@ -115,8 +125,23 @@ int main()
             }
         }
 
+        // update nebula animation frame
+        neb2RunningTime += dT;
+        if (neb2RunningTime >= neb2UpdateTime)
+        {
+            neb2RunningTime = 0.0;
+            neb2Rec.x = neb2Frame * neb2Rec.width;
+            neb2Frame++;
+            if (neb2Frame > 7)
+            { 
+                neb2Frame = 0;
+            }
+        }
+
         // draw nebula
         DrawTextureRec(nebula, nebRec, nebPos, WHITE);
+        // draw the second nebula
+        DrawTextureRec(nebula, neb2Rec, neb2Pos, RED);
 
         // draw scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
