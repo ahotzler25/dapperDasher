11f173ab21a56394961ba8f640ead5cb57490e04
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-05-25 12:47:58 -0600

    25 - Scrolling Background

--- dasher.cpp
+++ dasher.cpp
@@ -88,6 +88,9 @@ int main()
 
     int velocity{0};
 
+    Texture2D background = LoadTexture("textures/far-buildings.png");
+    float bgX{};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -97,6 +100,19 @@ int main()
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        // Scroll background
+        bgX -= 20 * dT;
+        if (bgX <= -background.width*2)
+        {
+            bgX = 0.0;
+        }
+
+        // draw the background
+        Vector2 bg1Pos{bgX, 0.0};
+        DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
+        Vector2 bg2Pos{bgX + background.width*2, 0.0};
+        DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
 
         // perform ground check
         if (isOnGround(scarfyData, windowDimensions[1]))
@@ -153,5 +169,6 @@ int main()
     }
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
+    UnloadTexture(background);
     CloseWindow();
 }
