ba580e46d22c06a24b6dd2be06db81913d7592a1
Stephen Ulibarri <druidmech@gmail.com>
2021-05-25 19:02:02 +0000

    Revert "26 - Duplicating The Background"
    This reverts commit 2d8c34ca412cde1ce9ee8cc9d4fb991f5b3291aa

--- dasher.cpp
+++ dasher.cpp
@@ -90,10 +90,6 @@ int main()
 
     Texture2D background = LoadTexture("textures/far-buildings.png");
     float bgX{};
-    Texture2D midground = LoadTexture("textures/back-buildings.png");
-    float mgX{};
-    Texture2D foreground = LoadTexture("textures/foreground.png");
-    float fgX{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -111,36 +107,12 @@ int main()
         {
             bgX = 0.0;
         }
-        // Scroll the midground
-        mgX -= 40 * dT;
-        if (mgX <= -midground.width*2)
-        {
-            mgX = 0.0;
-        }
-        // Scroll the midground
-        fgX -= 80 * dT;
-        if (fgX <= -foreground.width*2)
-        {
-            fgX = 0.0;
-        }
 
         // draw the background
         Vector2 bg1Pos{bgX, 0.0};
         DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
         Vector2 bg2Pos{bgX + background.width*2, 0.0};
         DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
-
-        // draw the midground
-        Vector2 mg1Pos{mgX, 0.0};
-        DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
-        Vector2 mg2Pos{mgX + midground.width*2, 0.0};
-        DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
-
-        // draw the foreground
-        Vector2 fg1Pos{fgX, 0.0};
-        DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
-        Vector2 fg2Pos{fgX + foreground.width*2, 0.0};
-        DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
 
         // perform ground check
         if (isOnGround(scarfyData, windowDimensions[1]))
@@ -198,7 +170,5 @@ int main()
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
     UnloadTexture(background);
-    UnloadTexture(midground);
-    UnloadTexture(foreground);
     CloseWindow();
 }
